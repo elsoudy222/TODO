67c9bc8fb6dbf2383a7ce1e57e290f3ad6f750f9
Said Elsoudy <elsoudy222@gmail.com>
2021-12-30 16:47:41 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: Colors.deepOrange
+          primarySwatch: Colors.teal
       ),
 
       home: Todo(),
